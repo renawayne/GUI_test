bd36e57da6afdfdaff8dfd2b6fb8349e65654751
RenaWayne <rena8713wayne@gmail.com>
2023-11-05 01:33:39 +0300

    work
    Signed-off-by: RenaWayne <rena8713wayne@gmail.com>

--- main.gd
+++ main.gd
@@ -1,15 +1,40 @@
 extends Node2D
+var penis = Penis.new()
+var dic1 = {
+	1: {"color": "red"},
+	2: {"color": "black"}
+}
+
+var dic2 = {
+	1: {"color": "red",
+		"name": "tomat"},
+	2: {"color": "blue",
+		"name": "chery"}
+}
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_action_pressed("exit"):
+	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
+		pass
+	if Input.is_action_just_pressed("pinis"):
+		penis.ready()
+		pass
+	if Input.is_action_just_pressed("pizda"):
+		Glob.ppp.emit()
+		pass
 	pass
+
+func temp():
+	for i in dic2:
+		if dic2[i]["color"] == "red":
+			print(dic2[i]["name"])
+
 
---
+++ pinis.gd
@@ -0,0 +1,11 @@
+class_name Penis
+#Glob.ppp.connect(ping())
+
+func ready():
+	print_rich("[color=red]Pinis.gd Загружается...[/color]")
+	await Glob.ppp
+	print_rich("[color=red]Pinis.gd Загружаен!!![/color]")
+	pass # Replace with function body.
+
+func ping():
+	print_rich("[color=red]Pinis.gd PING! PONG![/color]")
