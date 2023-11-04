d5eb7577df146ec7675081e182c3a3ac366bfa09
RenaWayne <rena8713wayne@gmail.com>
2023-11-04 05:49:58 +0300

    Новые функции и обработка BBcode

--- gui.gd
+++ gui.gd
@@ -1,15 +1,22 @@
 extends CanvasLayer
 
+var x = "Какойто рандомный текст с ошибкой"
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-#	print($Inventory.visible)
+#	push_error("Ошибка(красный текст): ", x)
+#	push_warning("Предупреждение(желтый текст): ", x)
+#	print("Standart print")
+#	print_rich("[bgcolor=red][color=green][b]Hello world![/b][/color][/bgcolor]")
+	
+	$windowsLable.text = "Process"
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	$windowsLable.text = "Process"
+	pass
+	
+	
 	if Input.is_action_just_pressed("tab"):
 		$Inventory.visible = !$Inventory.visible
 		print($Inventory.visible)
